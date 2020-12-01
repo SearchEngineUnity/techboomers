5b73b7bdd8e0189069faf9ea9b5b1b6f6f30e31b
sc-searchengineunity <sharon@searchengineunity.com>
2020-12-01 08:53:08 -0500

    adding preview action

--- studio/resolveDocumentActions.js
+++ studio/resolveDocumentActions.js
@@ -3,7 +3,7 @@ import defaultResolve from 'part:@sanity/base/document-actions';
 import PreviewAction from './previewAction';
 
 export default function resolveDocumentActions(props) {
-  if (props.type === 'page') {
+  if (props.type === 'page' || props.type === 'mpGuide' || props.type === 'guide') {
     return [PreviewAction, ...defaultResolve(props)];
   }
   return [...defaultResolve(props)];
