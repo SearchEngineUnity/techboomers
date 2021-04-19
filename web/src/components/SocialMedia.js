617c80495de030c52d4941f96525e31fd5d27db3
sc-searchengineunity <sharon@searchengineunity.com>
2021-04-19 14:07:58 -0400

    partially done footer

--- web/src/components/SocialMedia.js
+++ web/src/components/SocialMedia.js
@@ -1,8 +1,54 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Icon } from '@material-ui/core';
+import { IconButton } from 'gatsby-theme-material-ui';
 import { StaticQuery, graphql } from 'gatsby';
+import { keys } from '@material-ui/core/styles/createBreakpoints';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import PinterestIcon from '@material-ui/icons/Pinterest';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
-const ContactInfo = ({ data }) => <Typography>this is an address</Typography>;
+const ContactInfo = ({ data }) => {
+  const { allSanitySocialInfo: socials } = data;
+
+  return (
+    <>
+      {socials.edges.map((social) => {
+        const iconSelector = (key) => {
+          switch (key) {
+            case 'facebook':
+              return <FacebookIcon />;
+            case 'twitter':
+              return <TwitterIcon />;
+            case 'instagram':
+              return <InstagramIcon />;
+            case 'pinterest':
+              return <PinterestIcon />;
+            case 'linkedin':
+              return <LinkedInIcon />;
+            case 'youtube':
+              return <YouTubeIcon />;
+
+            default:
+              return <div>under construction</div>;
+          }
+        };
+        return (
+          <IconButton
+            color="inherit"
+            to={social.node.link}
+            target="_blank"
+            key={social.node.social}
+          >
+            {iconSelector(social.node.social)}
+          </IconButton>
+        );
+      })}
+    </>
+  );
+};
 export default function Contact(props) {
   return (
     <StaticQuery
