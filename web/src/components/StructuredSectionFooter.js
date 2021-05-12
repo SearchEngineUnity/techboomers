c24700a9f4e5520a30d5ba5bf213e2970e5b29a6
holly-searchengineunity <holly@searchengineunity.com>
2021-05-12 14:52:20 -0400

    spacing mq added for SLRFlex on mobile

--- web/src/components/StructuredSectionFooter.js
+++ web/src/components/StructuredSectionFooter.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Box } from '@material-ui/core';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Footer from './block-contents/SectionFooterSerializer';
-// import { mapSectionFooterToProps } from '../lib/mapToProps';
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    [theme.breakpoints.down('sm')]: {
+      marginTop: 16,
+    },
+  },
+}));
 
 function StructuredSectionFooter({ footer, colorOverrides, align, hasSectionFooter }) {
+  const classes = useStyles();
   return (
     <>
       {!hasSectionFooter && footer && (
         <Box
+          className={classes.footer}
           component={hasSectionFooter ? 'div' : 'footer'}
           color={colorOverrides?.footer?.hex || colorOverrides?.foreground?.hex || 'text.primary'}
-          mt={2}
+          mt={4}
           textAlign={align}
         >
           <Footer blocks={footer} />
