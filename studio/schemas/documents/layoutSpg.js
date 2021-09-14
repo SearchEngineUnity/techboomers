5f38bf825e9a5404e9ca2d913d1fd3c3ae7a18f3
sc-searchengineunity <sharon@searchengineunity.com>
2021-09-14 16:55:34 -0400

    adding layout for SPG

---
+++ studio/schemas/documents/layoutSpg.js
@@ -0,0 +1,49 @@
+import { MdBusiness } from 'react-icons/md';
+
+export default {
+  name: 'layoutSpg',
+  title: 'SPG layout',
+  type: 'document',
+  __experimental_actions: ['create', 'update', 'publish'],
+  fields: [
+    {
+      name: 'heroLrAlignment',
+      title: 'Hero LR Block Alignment',
+      type: 'string',
+      description: 'This determines how the L and R blocks align vertically to each other.',
+      options: {
+        list: [
+          { title: 'Top', value: 'flex-start' },
+          { title: 'Center', value: 'center' },
+        ],
+        layout: 'radio',
+        direction: 'horizontal',
+      },
+      initialValue: 'center',
+    },
+    {
+      name: 'heroImgAlignment',
+      title: 'Hero Image Alignment within block',
+      type: 'string',
+      description: 'This determines how the image aligns horizontally hwithin the right block.',
+      options: {
+        list: [
+          { title: 'Left', value: 'flex-start' },
+          { title: 'Center', value: 'center' },
+          { title: 'Right', value: 'flex-end' },
+        ],
+        layout: 'radio',
+        direction: 'horizontal',
+      },
+      initialValue: 'center',
+    },
+  ],
+  preview: {
+    select: {},
+    prepare() {
+      return {
+        title: `SPG Hero Layout`,
+      };
+    },
+  },
+};
