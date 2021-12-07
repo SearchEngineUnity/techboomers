be7a6a5ec1c016b8e9ef0cbdf85c9db45a9c07a0
sc-searchengineunity <sharon@searchengineunity.com>
2021-12-07 11:19:54 -0500

    remove unused style for TileSpgImageLeft.js

--- web/src/components/listingTile/TileSpgImageLeft.js
+++ web/src/components/listingTile/TileSpgImageLeft.js
@@ -7,27 +7,8 @@ import { CardActionArea } from 'gatsby-theme-material-ui';
 import sanityConfig from '../../../sanityConfig';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
   card: {
     height: '100%',
-  },
-  details: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-  },
-  content: {
-    flex: '1',
-  },
-  cover: {
-    minWidth: 400,
-  },
-  footer: {
-    [theme.breakpoints.down('md')]: {
-      marginTop: 16,
-    },
   },
 }));
 
@@ -45,25 +26,6 @@ function TileImageRecSqr({ image, alt, url, title, text, date }) {
   );
 
   return (
-    // <Card elevation={8} classes={{ root: classes.card }} square>
-    //   {/* the link probably cannot be spread in the same way as from regular tiles */}
-    //   <CardActionArea to={`/${url}`}>
-    //     <div className={classes.details}>
-    //       <GatsbyImage image={imageData} alt={alt} className={classes.cover} />
-    //       <CardContent className={classes.content}>
-    //         <Typography gutterBottom variant="h2">
-    //           {title}
-    //         </Typography>
-    //         <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
-    //           {lastUpdatedDate.toLocaleDateString('en-US', options)}
-    //         </Typography>
-    //         <Typography variant="body2" component="p">
-    //           {text}
-    //         </Typography>
-    //       </CardContent>
-    //     </div>
-    //   </CardActionArea>
-    // </Card>
     <Card elevation={8} classes={{ root: classes.card }} square>
       {/* the link probably cannot be spread in the same way as from regular tiles */}
       <CardActionArea to={`/${url}`}>
