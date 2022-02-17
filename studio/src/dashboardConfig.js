3d042530cf4163a68a2812ccebde5020477f3d38
sc-searchengineunity <sharon@searchengineunity.com>
2022-02-17 14:58:34 -0500

    updating netlify link / preview link / connections in studio

--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -1,25 +1,25 @@
 export default {
   widgets: [
-    // {
-    //   name: 'netlify',
-    //   options: {
-    //     title: 'My Netlify deploys',
-    //     sites: [
-    //       {
-    //         title: 'Site Builder V1',
-    //         apiId: 'bcacf036-c46b-4b68-a17b-88732d2e8d7c',
-    //         buildHookId: '60b667e99acf0500eabf9e8f',
-    //         name: 'democentermui',
-    //       },
-    //     ],
-    //   },
-    // },
-    // {
-    //   name: 'gatsby',
-    //   options: {
-    //     title: 'Gatsby Preview Site',
-    //     sites: [{ siteUrl: 'https://preview-sitebuildermui.gtsb.io/' }],
-    //   },
-    // },
+    {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'Site Builder V1',
+            apiId: '4fe5d933-672d-47d0-828b-dd08600061ac',
+            buildHookId: '620e8cb23e949e1921ab9548',
+            name: 'sitebuilderv1',
+          },
+        ],
+      },
+    },
+    {
+      name: 'gatsby',
+      options: {
+        title: 'Gatsby Preview Site',
+        sites: [{ siteUrl: 'https://preview-sitebuildermuiv1.gtsb.io/' }],
+      },
+    },
   ],
 };
