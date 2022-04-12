676ecb3dcffd39e8d395ccd8c034ef9d2975e7bd
sc-searchengineunity <sharon@searchengineunity.com>
2022-04-12 14:20:57 -0400

    moving to gatsby cloud preview extension

--- studio/resolveDocumentActions.js
+++ studio/resolveDocumentActions.js
@@ -3,8 +3,10 @@
 // import the default document actions
 import defaultResolve from 'part:@sanity/base/document-actions';
 
+import { gatsbyPreviewAction } from 'sanity-plugin-gatsby-cloud-preview';
 import { PreviewAction } from './PreviewAction';
 
 export default function resolveDocumentActions(props) {
-  return [PreviewAction, ...defaultResolve(props)];
+  // return [PreviewAction, ...defaultResolve(props)];
+  return [...defaultResolve(props), gatsbyPreviewAction];
 }
