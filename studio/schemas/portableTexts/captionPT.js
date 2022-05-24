e10f38889eb1299ee8f6be890f2f34edc91f49fe
sc-searchengineunity <sharon@searchengineunity.com>
2022-05-24 15:36:28 -0400

    making required work properly for tile image and hero image

---
+++ studio/schemas/portableTexts/captionPT.js
@@ -0,0 +1,122 @@
+import { FaExternalLinkAlt, FaLink } from 'react-icons/fa';
+import { GiLinkedRings } from 'react-icons/gi';
+import { MdLink } from 'react-icons/md';
+import ExternalLinkRenderer from '../components/previews/ExternalLinkRenderer';
+import InternalLocalRenderer from '../components/previews/InternalLocalRenderer';
+import InternalGlobalRenderer from '../components/previews/InternalGlobalRenderer';
+import JumpLinkRenderer from '../components/previews/JumpLinkRenderer';
+
+export default {
+  name: 'captionPT',
+  type: 'array',
+  of: [
+    {
+      type: 'block',
+      styles: [],
+      lists: [],
+      marks: {
+        annotations: [
+          {
+            name: 'internalLocal',
+            type: 'object',
+            title: 'Internal Local Link',
+            blockEditor: {
+              icon: FaLink,
+              render: InternalLocalRenderer,
+            },
+            fields: [
+              {
+                name: 'reference',
+                type: 'reference',
+                title: 'Reference',
+                to: [{ type: 'page' }, { type: 'soloGuidePage' }, { type: 'flexListingPage' }],
+              },
+              {
+                name: 'hashId',
+                title: 'Hash Id',
+                type: 'string',
+                description: 'Please enter the ID of the section you would like to jump to',
+              },
+              {
+                name: 'parameter',
+                title: 'Parameter(s)',
+                type: 'string',
+                description: 'Please enter all needed parameters for the link',
+              },
+              {
+                title: 'Open in new tab?',
+                name: 'newTab',
+                type: 'boolean',
+                initialValue: false,
+                validation: (Rule) => [Rule.required().error('Field is required')],
+              },
+            ],
+          },
+          {
+            name: 'internalGlobal',
+            type: 'object',
+            title: 'Internal Global Link',
+            blockEditor: {
+              icon: GiLinkedRings,
+              render: InternalGlobalRenderer,
+            },
+            fields: [
+              {
+                title: 'URL',
+                name: 'href',
+                type: 'url',
+                validation: (Rule) =>
+                  Rule.uri({
+                    allowRelative: false,
+                    scheme: ['https', 'http'],
+                  }),
+              },
+              {
+                title: 'Open in new tab?',
+                name: 'newTab',
+                type: 'boolean',
+                initialValue: false,
+                validation: (Rule) => [Rule.required().error('Field is required')],
+              },
+            ],
+          },
+          {
+            title: 'External Link',
+            name: 'externalLink',
+            type: 'object',
+            blockEditor: {
+              icon: FaExternalLinkAlt,
+              render: ExternalLinkRenderer,
+            },
+            fields: [
+              {
+                title: 'URL',
+                name: 'href',
+                type: 'url',
+                validation: (Rule) =>
+                  Rule.uri({
+                    allowRelative: false,
+                    scheme: ['https', 'http', 'mailto', 'tel'],
+                  }),
+              },
+              {
+                title: 'Open in new tab?',
+                name: 'newTab',
+                type: 'boolean',
+                initialValue: true,
+                validation: (Rule) => [Rule.required().error('Field is required')],
+              },
+              {
+                title: 'rel=noreferrer?',
+                name: 'noreferrer',
+                type: 'boolean',
+                initialValue: false,
+                validation: (Rule) => [Rule.required().error('Field is required')],
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+};
