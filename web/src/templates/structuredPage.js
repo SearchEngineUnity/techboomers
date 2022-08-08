54cd2c055730a1d7d14f276ea5e0b1aa1c557473
sc-searchengineunity <sharon@searchengineunity.com>
2022-08-08 10:54:52 -0400

    updating code so that section block text alignment option is working

--- web/src/templates/structuredPage.js
+++ web/src/templates/structuredPage.js
@@ -355,6 +355,7 @@ export const query = graphql`
               }
               _rawFooter(resolveReferences: { maxDepth: 10 })
               headerAlignment
+              textAlignment
               footerAlignment
             }
             ... on SanityVideoBlock {
