3f394d6706cec06831fd1d617692c0aa951be0cb
sc-searchengineunity <sharon@searchengineunity.com>
2022-09-12 09:56:44 -0400

    update mx for insertable on mobile

--- web/src/components/portableText/insertable/InsertableWrapper.js
+++ web/src/components/portableText/insertable/InsertableWrapper.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 
 const InsertableWrapper = ({ children }) => (
-  <Box mx={5} my={3}>
+  <Box mx={{ xs: 0, sm: 5 }} my={3}>
     {children}
   </Box>
 );
