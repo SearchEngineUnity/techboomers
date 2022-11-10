20cc4bc25b8944e9e22e0afc833732a84f984f60
sc-searchengineunity <sharon@searchengineunity.com>
2022-11-09 23:22:20 -0500

    fix: trying to urse order instead or flex direction

--- web/src/pages/row-reverse.js
+++ web/src/pages/row-reverse.js
@@ -6,15 +6,15 @@ const RowReverse = () => (
   <Container>
     <Box my={3}>
       <Container maxWidth="lg">
-        <Grid container spacing={3} direction="row-reverse">
+        <Grid container spacing={3}>
           <Hidden smDown>
-            <Grid item md={3}>
+            <Grid item md={3} style={{ order: 2 }}>
               <Box p={5} fontSize="42px" color="red" border="solid 5px red">
                 1
               </Box>
             </Grid>
           </Hidden>
-          <Grid item md={9} xs={12} component="article">
+          <Grid item md={9} xs={12} component="article" style={{ order: 1 }}>
             <Box p={5} fontSize="42px" color="blue" border="solid 5px blue">
               2
             </Box>
