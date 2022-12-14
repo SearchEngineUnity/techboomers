ea04b02b3d333e200eed627a9ceb471bf206876a
sc-searchengineunity <sharon@searchengineunity.com>
2022-12-14 10:32:47 -0500

    implement gatsby-plugin-layout

--- web/src/templates/flexListingPage.js
+++ web/src/templates/flexListingPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Layout from '../containers/layout';
 import Seo from '../components/Seo';
 import LrHero from '../components/sections/LrFlexHero';
 import LrFlex from '../components/sections/StructuredLrFlex';
@@ -1959,7 +1958,7 @@ export const query = graphql`
   }
 `;
 
-const FlexListingPage = ({ data, location, pageContext }) => {
+const FlexListingPage = ({ data, pageContext }) => {
   let allListItems;
   const spGuides = useSpGuides();
 
@@ -1979,7 +1978,7 @@ const FlexListingPage = ({ data, location, pageContext }) => {
   const listingItems = allListItems.slice((currentpage - 1) * limit, currentpage * limit);
 
   return (
-    <Layout location={location}>
+    <>
       <Seo {...mapSeoToProps(data.page, type)} />
       <main>
         {data.page.sections.map((section) => {
@@ -2012,7 +2011,7 @@ const FlexListingPage = ({ data, location, pageContext }) => {
           }
         })}
       </main>
-    </Layout>
+    </>
   );
 };
 
