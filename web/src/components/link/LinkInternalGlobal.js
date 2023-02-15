61f01f287313d27512f79d9e0977cdaadcb47bda
holly-searchengineunity <holly@searchengineunity.com>
2023-02-14 21:42:40 -0500

    added underline to all links except jumplinks

--- web/src/components/link/LinkInternalGlobal.js
+++ web/src/components/link/LinkInternalGlobal.js
@@ -8,6 +8,7 @@ function InternalGlobal({ href, children, newTab, className }) {
       rel={newTab ? 'noopener' : undefined}
       href={href}
       className={className}
+      underline="always"
     >
       {children}
     </Link>
